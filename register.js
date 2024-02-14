3bafecf0bdbaf18716ea9d5ac9a59d10dfe735c9
rishi-d3v <rishi@d3vtech.com>
2024-02-15 03:14:01 +0530

    form validation bug fixed

--- register.js
+++ register.js
@@ -5,7 +5,7 @@ function validateForm() {
   const username = usernameInput.value;
   const password = passwordInput.value;
 
-  if (username.length > 5 || password.length > 5) {
+  if (username.length < 5 || password.length < 5) {
     console.log("Username and password must be at least 5 characters long.");
     return false;
   } else {
